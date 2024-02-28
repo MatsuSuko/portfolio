80663bccb784faa63706aaceaf6ea2ad0df02c83
Souvanny Bounmy <souvannyb21@gmail.com>
2024-02-28 16:06:04 +0100

    correction de bug

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,5 +1,6 @@
 import ImgPhotoProfil from "@/../public/imgPhotoProfil.jpg"
 import Image from "next/image";
+
 export default function About() {
     return (
         <div className="flex flex-col w-full">
@@ -11,16 +12,17 @@ export default function About() {
                 <div className="grid grid-cols-2 gap-2">
                     <div className="p-4 border rounded-lg shadow-xl bg-slate-50">
                         <h1 className="text-xl font-medium mb-8 text-center">
-                            Hey ! je suis Souvanny BOUNMY, un développeur web (fullstack)
+                            Hey ! Je suis Souvanny BOUNMY et je suis un développeur web (fullstack)
                         </h1>
                         <p>
-                            Ma passion pour l&apos;informatique, surtout le développement, m&apos;a toujours intéressé et fasciné.
-                            Après plusieurs stages dans le domaine de l&apos;informatique, j&apos;ai vraiment compris que c&apos;était
-                            le domaine du développement où je voulais être. Suite à ça j&apos;ai fait un bac STI2D
-                            spécialisée en informatique et réseaux. Actuellement, je suis en bachelor spécialisé en
-                            développement, je veux continuer mes études en mastère dans ce même domaine. Je suis
-                            convaincu que cela me permettra d&apos;approfondir mes compétences et de me préparer pour une
-                            carrière dans le développement informatique.
+                            Ma passion pour l&apos;informatique est née dès mon plus jeune âge, et elle m&apos;a toujours
+                            intrigué et captivé. Après avoir effectué plusieurs stages dans ce domaine, j&apos;ai pleinement
+                            réalisé que le développement informatique était la voie que je souhaitais suivre. C&apos;est
+                            pourquoi j&apos;ai choisi de poursuivre mes études avec un Bac STI2D spécialisé en informatique
+                            et réseaux. Actuellement engagé dans un Bachelor spécialisé en développement, mon objectif
+                            est de continuer vers un Master dans ce même domaine. Je suis convaincu que cette
+                            progression me permettra d&apos;approfondir mes compétences et de me préparer efficacement pour
+                            une carrière dans le développement informatique.
                         </p>
                     </div>
                     <div className="flex justify-center items-center">
