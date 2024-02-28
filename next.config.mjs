96e6d03d1574a151b22ce018fc163ac9c89458ab
Souvanny Bounmy <souvannyb21@gmail.com>
2024-02-28 02:01:01 +0100

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    distDir: 'dist',
+    output: 'export',
+    images: {
+        loader: 'custom',
+        loaderFile: './my-loader.ts',
+    },
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+};
 
 export default nextConfig;
